6b784a3504ada7cd7770c057e1fa90ff008a3fb0
DaoQuyet <nmhtdyt@gmail.com>
2024-01-13 00:10:23 +0700

    no message

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 ### Khởi chạy ứng dụng
 Nhập lệnh sau trên cửa sổ Terminal
-
-``` bash
 python .
-```
-hoặc
-``` bash
-python __main__.py
-```
--- __main__.py
+++ __main__.py
@@ -1,5 +1,4 @@
 from Backend.Database.db_sessions import *
-from Backend.Services.Scanner.fake_scanner import *
 from Frontend.Helper.helper import *
 from Frontend.Design.design_ui import Ui_MainWindow
 from collections import defaultdict
